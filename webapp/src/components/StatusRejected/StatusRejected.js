bb71d08acc32088d3eb8df4b2bac92ee5cf4a47c
kathu08 <sivalingamk.19@uom.lk>
2023-05-17 18:37:53 +0530

    Display reason for rejection

--- webapp/src/components/StatusRejected/StatusRejected.js
+++ webapp/src/components/StatusRejected/StatusRejected.js
@@ -13,41 +13,45 @@ import Navbar from "../Navbar/Navbar";
 import { Link } from "react-router-dom";
 
 function StatusRejected() {
+  // Get reason for rejection
+  const queryParams = new URLSearchParams(window.location.search);
+  const reason = queryParams.get('reason');
+
   const API_HOST =
-  "https://f82fbb50-01e1-4078-a9f8-0d4ed79a518a-dev.e1-us-east-azure.choreoapis.dev/sbmq/grama-check/requestservice-369/1.0.0";
+    "https://f82fbb50-01e1-4078-a9f8-0d4ed79a518a-dev.e1-us-east-azure.choreoapis.dev/sbmq/grama-check/requestservice-369/1.0.0";
 
-// Asgardeo access token
-const TOKEN = JSON.parse(
-  sessionStorage.getItem("session_data-instance_0")
-).access_token;
-const updateStatus = () => {
-  console.log("Update Status");
-const email = JSON.parse(localStorage.getItem("user")).email;
-const requestId = JSON.parse(localStorage.getItem("requestId")).requestId;
-  const url = API_HOST+"/request/"+requestId;
-const requestStatus={
-requestId:requestId,
-status:"completed"
-}
-  var requestOptions = {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + TOKEN,
-    },
-    body: JSON.stringify(requestStatus),
-    redirect: "follow",
+  // Asgardeo access token
+  const TOKEN = JSON.parse(
+    sessionStorage.getItem("session_data-instance_0")
+  ).access_token;
+  const updateStatus = () => {
+    console.log("Update Status");
+    const email = JSON.parse(localStorage.getItem("user")).email;
+    const requestId = JSON.parse(localStorage.getItem("requestId")).requestId;
+    const url = API_HOST + "/request/" + requestId;
+    const requestStatus = {
+      requestId: requestId,
+      status: "completed"
+    }
+    var requestOptions = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + TOKEN,
+      },
+      body: JSON.stringify(requestStatus),
+      redirect: "follow",
+    };
+
+    fetch(url, requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          console.log("Request status updated successfully");
+          window.location.href = "/request-form";
+        }
+      })
+      .catch((error) => console.log(error));
   };
-
-  fetch(url, requestOptions)
-    .then((response) => {
-      if (response.ok) {
-        console.log("Request status updated successfully");
-        window.location.href = "/request-form";
-      }
-    })
-    .catch((error) => console.log(error));
-};
   return (
     <div className="statusRejected">
       <div className="statusRejected__container">
@@ -65,6 +69,8 @@ status:"completed"
             <p>
               Sorry! Your request for the document has been rejected by grama
               niladari.
+              <br /><br />
+              <b>Reason - {reason}</b>
             </p>
           </div>
           <div className="statusRejected__content__buttons">
