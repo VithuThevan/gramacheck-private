875a0ab6aad471635f9eb02e23e3e87436e011ce
kathu08 <sivalingamk.19@uom.lk>
2023-05-23 14:24:39 +0530

    Update user navigation

--- webapp/src/pages/LoaderPage/LoaderPage.js
+++ webapp/src/pages/LoaderPage/LoaderPage.js
@@ -1,13 +1,50 @@
 /* ----- LoaderPage.js ----- */
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-// Components
-import Loader from "../../components/Loader/Loader";
+// Libraries & Packages
+import { useAuthContext } from "@asgardeo/auth-react";
+
+import GetStartedPage from "../GetStartedPage/GetStartedPage";
+import GramaSevakaDashboard from "../GramaSevakaDashboardPage/GramaSevakaDashboardPage";
 
 function LoaderPage() {
+  // Asgardeo Auth Context
+  const { state, getBasicUserInfo } = useAuthContext();
+  const [isGS, setIsGS] = useState();
+
+  useEffect(() => {
+    const getBasicUserDetails = () => {
+      if (state.isAuthenticated) {
+        getBasicUserInfo()
+          .then((basicUserDetails) => {
+            const user = {
+              firstName: basicUserDetails.givenName,
+              lastName: basicUserDetails.familyName,
+              email: basicUserDetails.email,
+              group: basicUserDetails.groups,
+            };
+            localStorage.setItem("user", JSON.stringify(user));
+            if (user.group !== undefined) {
+              if (user.group[0] === "GramaSevaka") {
+                setIsGS(true);
+              } 
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else {
+        localStorage.removeItem("user");
+        localStorage.removeItem("requestId");
+      }
+    }
+    getBasicUserDetails();
+  }, [getBasicUserInfo, state.isAuthenticated])
+
   return (
     <>
-      <Loader />
+      {isGS === true ? <GramaSevakaDashboard/> : <GetStartedPage/>
+      }
     </>
   );
 }
